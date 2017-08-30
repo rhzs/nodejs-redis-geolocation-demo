ad24d556724d3b30b7eec7b5157a8b9a308a8830
rheza <rheza.satria.ta@gmail.com>
2017-08-31 00:01:11 +0700

    test: simplify test

--- tests/unit/test-geo-random-generator.js
+++ tests/unit/test-geo-random-generator.js
@@ -1,10 +1,9 @@
 import test from 'ava'
 import jakarta from '../fixture/jakarta-lat-long.js'
-import {isLessThanEqualTo} from '../helper/test-operations.js'
 import generateMapPoints from '../../lib/geo-random-generator'
 
 test('Testing generate random map points within radius: generateMapPoints()', t => {
-  t.plan(1)
+  t.plan(11)
 
   const amount = 10
   const radius = parseFloat(100)
@@ -13,6 +12,6 @@ test('Testing generate random map points within radius: generateMapPoints()', t 
   t.is(generatedPoints.length, 10)
 
   generatedPoints.forEach(({distance}, index) => {
-    isLessThanEqualTo(parseFloat(distance), radius)
+    t.true(parseFloat(distance) <= radius)
   })
 })
